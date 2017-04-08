15ed95999cbd196b683bd9c068d1814804d55179
Wen Kokke <wenkokke@users.noreply.github.com>
2017-04-08 14:34:32 +0100

    Changed create MCB script.

--- create_mcb_data.py
+++ create_mcb_data.py
@@ -12,34 +12,40 @@ import tqdm
 def create_toks_dict(toks_list):
     toks_list = sorted(toks_list)
     return {k:list(sorted(v,key=len,reverse=True))
-            for k,v in itertools.groupby(toks_list, key=lambda ln: ln[0])}
+            for k,v in itertools.groupby(toks_list, key=lambda ln: ln[0])
+            if k and k.strip()}
 
 def toks_tokenise_line(line,toks_dict,start=0):
     toks = []
     while start < len(line) - 1:
-        for tok in toks_dict[line[start]]:
-            if line.startswith(tok,start):
-                start += len(tok)
-                toks.append(tok)
-                break
+        if line[start] and line[start].strip():
+            for tok in toks_dict[line[start]]:
+                if line.startswith(tok,start):
+                    start += len(tok)
+                    toks.append(tok)
+                    break
+        else:
+            toks.append(' ')
+            start += 1
     return toks
 
 def toks_tokenise_text(text,toks_dict):
     count = collections.Counter()
     for line in text:
-        toks = toks_tokenise_line(line,toks_dict)
+        toks = toks_tokenise_line(line.strip(),toks_dict)
         for bigram in zip(toks,toks[1:]):
             count[bigram] += 1
     return count
 
 def create_toks_list(text,max_size):
-    text = [line.replace(' '*1,' '*0).strip() for line in text]
     toks_list = set(itertools.chain(*[line.replace(' '*0,' '*1).strip().split() for line in text]))
     toks_dict = create_toks_dict(toks_list)
     start_size = len(toks_list)
     for _ in tqdm.tqdm(range(start_size,max_size)):
         count = toks_tokenise_text(text,toks_dict)
-        most_common = ''.join(count.most_common()[:1][0][0])
+        most_common = next(x + y \
+            for (x, y), count in count.most_common()
+            if x and x.strip() and y and y.strip())
         toks_dict[most_common[0]].append(most_common)
         toks_dict[most_common[0]].sort(key=len,reverse=True)
     return tuple(itertools.chain(*toks_dict.values()))
@@ -55,12 +61,12 @@ def mcb_tokenise_file(data_file_in,data_file_out,fmax_size,data_file_orig=None):
     else:
         text_orig = text_in
     max_size = fmax_size(len(set(itertools.chain(*[line.strip().split() for line in text_orig]))))
-    
+
     toks_list = create_toks_list(text_in,max_size)
     toks_dict = create_toks_dict(toks_list)
     with open(data_file_out, 'w') as text_out:
         for line in text_in:
-            text_out.write(' '.join(toks_tokenise_line(line.replace(' '*1,' '*0),toks_dict))+'\n')
+            text_out.write(' '.join(toks_tokenise_line(line,toks_dict))+'\n')
 
 def mcb_tokenise_dir(data_dir_in,data_dir_out,fmax_size,
                       text_en='text.en',text_fr='text.fr',
@@ -100,15 +106,31 @@ def mcb_tokenise_dir(data_dir_in,data_dir_out,fmax_size,
 
 
 # # Creating datasets
-# mcb_tokenise_dir(data_dir_in='in_en_data_50000',data_dir_out='in_en_data_mcb8_50000',
-#                  mcb_tokenise_en=False,fmax_size=lambda orig_size: orig_size // 8)
-mcb_tokenise_dir(data_dir_in='in_en_data_mbc8_50000',
-                 data_dir_out='in_en_data_mcb4_50000',
+
+mcb_tokenise_dir(data_dir_in='in_en_data_50000',
+                 data_dir_out='in_en_data_mcb128_50000',
                  mcb_tokenise_en=False,
-                 fmax_size=lambda orig_size: orig_size // 4,
-                 data_dir_orig='in_en_data_50000',)
-mcb_tokenise_dir(data_dir_in='in_en_data_mbc4_50000',
-                 data_dir_out='in_en_data_mcb2_50000',
+                 fmax_size=lambda orig_size: orig_size // 128)
+mcb_tokenise_dir(data_dir_in='in_en_data_mcb128_50000',
+                 data_dir_out='in_en_data_mcb64_50000',
                  mcb_tokenise_en=False,
-                 fmax_size=lambda orig_size: orig_size // 2,
-                 data_dir_orig='in_en_data_50000',)
+                 fmax_size=lambda orig_size: orig_size // 64,
+                 data_dir_orig='in_en_data_50000')
+mcb_tokenise_dir(data_dir_in='in_en_data_mcb64_50000',
+                 data_dir_out='in_en_data_mcb32_50000',
+                 mcb_tokenise_en=False,
+                 fmax_size=lambda orig_size: orig_size // 32,
+                 data_dir_orig='in_en_data_50000')
+mcb_tokenise_dir(data_dir_in='in_en_data_mcb32_50000',
+                 data_dir_out='in_en_data_mcb16_50000',
+                 mcb_tokenise_en=False,
+                 fmax_size=lambda orig_size: orig_size // 16,
+                 data_dir_orig='in_en_data_50000')
+mcb_tokenise_dir(data_dir_in='in_en_data_mcb16_50000',
+                 data_dir_out='in_en_data_mcb8_50000',
+                 mcb_tokenise_en=False,
+                 fmax_size=lambda orig_size: orig_size // 8,
+                 data_dir_orig='in_en_data_50000')
+
+
+
